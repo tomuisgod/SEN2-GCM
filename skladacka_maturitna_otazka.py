4007dc3ecf863f9a44225c9bba35e1e86a50ec66
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2023-10-09 09:31:45 +0200

    Create skladacka_maturitna_otazka.py

---
+++ skladacka_maturitna_otazka.py
@@ -0,0 +1,44 @@
+import random
+import tkinter
+
+canvas = tkinter.Canvas(width="300", height="300")
+
+# Pole
+a = 3
+sp = 300 / a
+p = [[1,2,3], [4,5,6], [7,8,0]] 
+
+# Tvorenie pola
+for i in range(1, a + 1):
+    canvas.create_line(sp * i, 0, sp * i, 700, width=5)
+    canvas.create_line(0, sp * i, 700, sp * i, width=5)
+
+for r in range(3):
+    for c in range(3):
+        a = p[r][c]
+        canvas.create_text(100*r + 50, 100*c + 50, text=str(a), fill="Red", font=('Helvetica 20 bold'))
+
+def klik(a):
+    print("[ X: ", a.x // 100, "| Y: ", a.y // 100,"]")
+
+    if r+1<3 and p[r+1][c] == 0:
+        canvas.move(p[r][c], 100, 0)
+        p[r][c],p[r+1][c] = p[r+1][c],p[r][c]
+
+    elif c+1<3 and p[r][c+1] == 0:
+        canvas.move(p[r][c], 0, 100)
+        p[r][c],p[r][c+1] = p[r][c+1],p[r][c]
+
+    elif r-1>3 and p[r-1][c] == 0:
+        canvas.move(p[r][c], -100, 0)
+        p[r][c],p[r-1][c] = p[r-1][c],p[r][c]
+    elif c-1>3 and p[r][c-1] == 0:
+        canvas.move(p[r][c], 0, -100)
+        p[r][c],p[r][c-1] = p[r][c-1],p[r][c]
+
+canvas.bind("<Button-1>", klik)
+
+# NENI HOTOVE
+
+canvas.pack()
+canvas.mainloop()
