209852e8b9af3f470eb932ef25f8045661dad5d3
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2023-10-23 09:13:11 +0200

    Update sopecky_maturitna_otazka.py

--- sopecky_maturitna_otazka.py
+++ sopecky_maturitna_otazka.py
@@ -1,1 +1,25 @@
+import tkinter
+import random
 
+canvas = tkinter.Canvas(width=300, height=300)
+canvas.pack()
+
+sopka = [0, 300]
+
+# c var urcuje skok
+vrcholx = random.randrange(0, 300, 10)
+
+for x in range(0, vrcholx, 10):
+    sopka.append(x)
+    sopka.append(300-random.randrange(x, x+10))
+
+for x in range(vrcholx, 300, 10):
+    sopka.append(x)
+    sopka.append(random.randrange(x, x+10))
+
+sopka.append(300)
+sopka.append(300)
+
+canvas.create_polygon(sopka)
+
+canvas.mainloop()
