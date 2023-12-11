04c5f972af0236b37e86fed267295c04d4790037
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2023-12-11 09:15:39 +0100

    Update pexeso_radio_express.py

--- pexeso_radio_express.py
+++ pexeso_radio_express.py
@@ -4,6 +4,7 @@ pr = []
 p = []
 pv = []
 pexeso = []
+pp = ["A", "B", "C", "D"]
 
 for i in range(1, 13):
     pv.append(i)
@@ -18,15 +19,39 @@ for i in range(0, 24, 6):
     pexeso.append(p[i:i + 6])
 
 for i in range(4):
-    pr.append(["0"]*6)
+    pr.append(["0"] * 6)
+
 
 def vypis_pola(a):
     for i in range(len(a)):
         print(a[i])
 
-vypis_pola(pr)
-print()
-vypis_pola(pexeso)
 
-while True:
-    pass
+def ukaz_pole():
+    vypis_pola(pr)
+    print()
+    vypis_pola(pexeso)
+
+test = True
+
+while test:
+    o = input("Zadaj súradnice npr. A6: ").upper()
+    pr[pp.index(o[0])][int(o[1])-1] = pexeso[pp.index(o[0])][int(o[1])-1]
+    vypis_pola(pr)
+
+    Q = input("Zadaj súradnice npr. A6: ").upper()
+    pr[pp.index(Q[0])][int(Q[1])-1] = pexeso[pp.index(Q[0])][int(Q[1])-1]
+    vypis_pola(pr)
+
+    if pexeso[pp.index(Q[0])][int(Q[1])-1] == pexeso[pp.index(o[0])][int(o[1])-1]:
+        pass
+    else:
+        pr[pp.index(o[0])][int(o[1]) - 1] = 0
+        pr[pp.index(Q[0])][int(Q[1]) - 1] = 0
+
+    test = False
+
+    for i in range(len(pr)):
+        if 0 in pr[i]:
+            test = True
+            break
