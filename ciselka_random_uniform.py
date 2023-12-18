f113deed5da86e04865d6f562c60e461a2bd07f7
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2023-12-18 09:08:35 +0100

    Create ciselka_random_uniform.py

---
+++ ciselka_random_uniform.py
@@ -0,0 +1,35 @@
+"""
+Maturitná otázka použitím modulu random a funckiue .uniform()... 
+Tento spôsob je negelálny a odporúčam array robiť manuálne,
+nie pomocou tohto kódu.
+"""
+
+import random
+
+neroztriedene = []
+neroz_naozaj = []
+n = int(input("Zadaj počet čísel: "))
+
+for i in range(n):
+    a = round(random.uniform(0.0, 10.0), 1)
+    float(a)
+    neroztriedene.append(a)
+
+neroz_naozaj = neroztriedene[:]
+
+def bbs_print(a):
+    for x in range(len(neroztriedene)):
+        for i in range(len(neroztriedene)-1-x):
+            if neroztriedene[i] > neroztriedene[i+1]:
+                neroztriedene[i],neroztriedene[i+1] = neroztriedene[i+1], neroztriedene[i]
+
+    return neroztriedene
+
+def med(a):
+    if len(neroztriedene) % 2 == 0:
+        return neroztriedene[len(neroztriedene) // 2], neroztriedene[len(neroztriedene) // 2 + 1] / 2
+    else:
+        return neroztriedene[len(neroztriedene) // 2]
+
+print(med(neroztriedene))
+print(bbs_print(neroztriedene))
