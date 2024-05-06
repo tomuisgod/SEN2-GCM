4e15392a6121bb1de7b9188d6bd8c678bae06bad
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2024-05-06 08:19:31 +0200

    Add files via upload

---
+++ vlckus_polygon_maturita.py
@@ -0,0 +1,13 @@
+import tkinter
+
+canvas = tkinter.Canvas(width=1000, height=1000)
+n = int(input("Zadajte pocet trojuholnikov: "))
+
+while n > 0:
+    canvas.create_polygon(499, 300-n*60, 328-n*52, 600+n*31, 675+n*28, fill="white", outline="black")
+    n -= 1
+
+canvas.pack()
+canvas.mainloop()
+
+input("")
