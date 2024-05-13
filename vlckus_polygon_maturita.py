049f78b89f6af7f98d014535f238bc0a0a101a64
Adam Vlčko <48559812+vlckoadam@users.noreply.github.com>
2024-05-13 23:07:19 +0200

    opravene

--- vlckus_polygon_maturita.py
+++ vlckus_polygon_maturita.py
@@ -4,7 +4,7 @@ canvas = tkinter.Canvas(width=1000, height=1000)
 n = int(input("Zadajte pocet trojuholnikov: "))
 
 while n > 0:
-    canvas.create_polygon(499, 300-n*60, 328-n*52, 600+n*31, 675+n*28, fill="white", outline="black")
+    canvas.create_polygon(499, 300-n*60, 328-n*52, 600+n*31, 675+n*51, 600+n*28, fill="white", outline="black")
     n -= 1
 
 canvas.pack()
